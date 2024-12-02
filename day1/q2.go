e3d5ee8d2d4c712086d09cd2cec2a484deb70787
Rakshit Sinha <rakshit.sinha@oracle.com>
2024-12-01 21:31:35 -0800

    Completed Day-1 Question 2

--- day1/q2.go
+++ day1/q2.go
@@ -3,5 +3,21 @@ package day1
 import "fmt"
 
 func q2() {
-	fmt.Println("hello from q2")
+	col1, col2 := setupInput()
+
+	// Create frequency maps for each of the columns
+	map1 := make(map[int]int)
+	map2 := make(map[int]int)
+
+	for i := 0; i < 1000; i++ {
+		map1[col1[i]] = map1[col1[i]] + 1
+		map2[col2[i]] = map2[col2[i]] + 1
+	}
+
+	similarityScore := 0
+	for num, freq := range map1 {
+		similarityScore += (num * map2[num] * freq)
+	}
+
+	fmt.Printf("Result for Day-1 Q2: %d\n", similarityScore)
 }
